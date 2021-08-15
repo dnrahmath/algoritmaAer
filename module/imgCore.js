51ae0e07427763c50dd3af27f1e988e8beaca34c
dn.rahmath <50563545+dnrahmath@users.noreply.github.com>
2021-08-15 09:31:57 +0700

    Update imgCore.js

--- module/imgCore.js
+++ module/imgCore.js
@@ -168,7 +168,7 @@ class imgClass {          //Class Model Memanggil seluruh function pada Class Ca
                     frameLooping();
                 }
     
-            }, 900);  // 0.75 fps
+            }, 1500);  // 0.75 fps
     
         }
     
